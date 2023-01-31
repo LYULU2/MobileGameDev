4f9be0f71685d74b8c0858baa8b183a1dd2c6ba7
studentBC <r04546032@ntu.edu.tw>
2023-01-30 21:27:54 -0800

    prototype 2 draft

---
+++ Assets/Script/moveForward.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveForward : MonoBehaviour
+{
+    public float speed = 5.0f;
+    private Vector2 target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = new Vector2(7.93f, -3.93f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        float step = speed * Time.deltaTime;
+
+        // move sprite towards the target location
+        transform.position = Vector2.MoveTowards(transform.position, target, step);
+    }
+}
