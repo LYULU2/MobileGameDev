dca8a6b698fb289443a8531eabd5939e7d009155
Zoraaaa <rongyu923826@outlook.com>
2023-02-06 00:16:40 -0800

    Added Shit function, fixed some small bugs

--- Assets/Script/PlayerBehaviour.cs
+++ Assets/Script/PlayerBehaviour.cs
@@ -7,6 +7,7 @@ using UnityEngine.UI;
 public class PlayerBehaviour : MonoBehaviour
 {
     public float decreaseDistance = 1.0f;
+    public float decreaseScale_Shit = 0.05f;
     public float decreaseScale = 0.05f;
     public float increaseScale = 0.05f;
     public float loseScale_Small = 0.01f;
@@ -14,8 +15,8 @@ public class PlayerBehaviour : MonoBehaviour
     public Text BlueCount;
     public Text YellowCount;
     public Text RedCount;
-    public int Blue = 1;
-    public int Yellow = 1;
+    public int Blue = 0;
+    public int Yellow = 0;
     public int Red = 1;
     public Color color;
     Vector3 posStart;
@@ -26,24 +27,26 @@ public class PlayerBehaviour : MonoBehaviour
     public float lastEatDistance = 0;
     public GameObject LoseScreen_Big;
     public GameObject LoseScreen_Small;
-    public GameObject player;
+    // public GameObject player;
     
     private void Start()
     {
         BlueCount.text = Blue.ToString();
         YellowCount.text = Yellow.ToString();
-        RedCount.text = Red.ToString();
+        // RedCount.text = Red.ToString();
         posStart = transform.position;
         oldPosition = transform.position;
         lastEatPosition = transform.position;
     }
     private void Update() {
+        checkShit(decreaseScale_Shit);
+
         //calculate player's walking distance
         Vector3 distanceVector = transform.position - oldPosition;
         float distance = distanceVector.magnitude;
         totalDistance += distance;
         
-        //calculate player's hungry distance
+        //calculate gameObject's hungry distance
         Vector3 distanceVector2 = transform.position - lastEatPosition;
         float distance2 = distanceVector2.magnitude;
         lastEatDistance = distance2;
@@ -52,11 +55,12 @@ public class PlayerBehaviour : MonoBehaviour
         // shrinkDistance = Vector2.Distance(posStart, transform.position);
         if (lastEatDistance >= decreaseDistance)
         {
-            transform.localScale -= new Vector3(decreaseScale, decreaseScale, decreaseScale);
-            // transform.localScale -= new Vector3(0.05f, 0.05f, 0.05f);
-            posStart = transform.position;
-            lastEatDistance = 0;
-            lastEatPosition = transform.position;
+            DecreaseScale(decreaseScale);
+            // transform.localScale -= new Vector3(decreaseScale, decreaseScale, decreaseScale);
+            // // transform.localScale -= new Vector3(0.05f, 0.05f, 0.05f);
+            // posStart = transform.position;
+            // lastEatDistance = 0;
+            // lastEatPosition = transform.position;
         }
         if (transform.localScale.x < loseScale_Small || transform.localScale.y < loseScale_Small || transform.localScale.z < loseScale_Small) {
             LoseScreen_Small.SetActive(true);
@@ -68,11 +72,32 @@ public class PlayerBehaviour : MonoBehaviour
             Destroy(gameObject);
         }
     }
+    private void checkShit(float decreaseScale_Shit)
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            Debug.Log("space key was pressed");
+            DecreaseScale(decreaseScale_Shit);
+            gameObject.GetComponent<SpriteRenderer>().color = Color.white;
+            // color = Color.white;
+            Debug.Log("space key was pressed");
+        }
+    }
+    public void DecreaseScale(float decreaseScale)
+    {
+        transform.localScale -= new Vector3(decreaseScale, decreaseScale, decreaseScale);
+        // transform.localScale -= new Vector3(0.05f, 0.05f, 0.05f);
+        posStart = transform.position;
+        lastEatDistance = 0;
+        lastEatPosition = transform.position;
+
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "Blue")
         {
             color = collision.GetComponent<SpriteRenderer>().color;
+            Blue = Blue + 1;
             BlueCount.text = Blue.ToString();
             collision.gameObject.SetActive(false);
             //player eat
@@ -83,6 +108,7 @@ public class PlayerBehaviour : MonoBehaviour
             //transform.localScale += new Vector3(0.05f, 0.05f, 0.05f);
         } else if (collision.tag == "Yellow") {
             color = collision.GetComponent<SpriteRenderer>().color;
+            Yellow = Yellow + 1;
             YellowCount.text = Yellow.ToString();
             collision.gameObject.SetActive(false);
             //player eat
