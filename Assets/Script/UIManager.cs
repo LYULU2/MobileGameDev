8ee2217d27687bb96b2717cf62bccc82bdfc7977
Zoraaaa <rongyu923826@outlook.com>
2023-02-27 15:50:22 -0800

    Added Pause Menu

--- Assets/Script/UIManager.cs
+++ Assets/Script/UIManager.cs
@@ -5,45 +5,92 @@ using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour
 {
-    public GameObject StartButton;
-    public GameObject SelectLevelButton;
-    public GameObject QuitButton;
+    public GameObject MainMenuButtons;
+    public GameObject PauseButton;
+    public GameObject PauseMenuButtons;
+    public GameObject MainMenuCanvas;
     public GameObject LevelSelectCanvas;
+    public GameObject SettingCanvas;
+    public GameObject PauseCanvas;
+    public GameObject WinCanvas;
+    public GameObject LoseCanvas;
     // Start is called before the first frame update
     void Start(){
-        BackToMain();
+        if (SceneManager.GetActiveScene().buildIndex == 0){
+            BackToMain();
+        }
+        else{
+            InGame();
+        }
     }
     public void GameStart(){
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
     }
 
     public void SelectLevel(){
-        StartButton.SetActive(false);
-        QuitButton.SetActive(false);
-        SelectLevelButton.SetActive(false);
-
+        // Buttons
+        MainMenuButtons.SetActive(false);
+        // Canvas
         LevelSelectCanvas.SetActive(true);
+        SettingCanvas.SetActive(false);
     }
 
-    void Setting(){
-
-    }
-
-    void ShowCredit(){
-
+    public void Setting(){
+        // Buttons
+        MainMenuButtons.SetActive(false);
+        // Canvas
+        LevelSelectCanvas.SetActive(false);
+        SettingCanvas.SetActive(true);
     }
 
     public void BackToMain(){
-        StartButton.SetActive(true);
-        QuitButton.SetActive(true);
-        SelectLevelButton.SetActive(true);
-
+        // Buttons
+        MainMenuButtons.SetActive(true);
+        // Canvas
+        MainMenuCanvas.SetActive(true);
         LevelSelectCanvas.SetActive(false);
+        SettingCanvas.SetActive(false);
+    }
+
+    public void BackToMainMenuFromGame(){
+        SceneManager.LoadScene("MainMenu");
     }
 
     public void QuitGame(){
         Application.Quit();
     }
+
+    public void InGame(){
+        Time.timeScale = 1f;
+        // Buttons
+        PauseButton.SetActive(true);
+        PauseMenuButtons.SetActive(false);
+        // Canvas
+        MainMenuCanvas.SetActive(false);
+        PauseCanvas.SetActive(false);
+        WinCanvas.SetActive(false);
+        LoseCanvas.SetActive(false);
+    }
+
+    public void Restart(){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void PauseMenu(){
+        Time.timeScale = 0f;
+        PauseCanvas.SetActive(true);
+        PauseButton.SetActive(false);
+        PauseMenuButtons.SetActive(true);
+    }
+
+    public void LoseMenu(){
+        LoseCanvas.SetActive(true);
+    }
+
+    public void WinMenu(){
+        WinCanvas.SetActive(true);     
+    }
+
 
     // Load levels
 
