bd4fd5d60ae08420308f70a8b24f6a2479c9b44a
Zoraaaa <rongyu923826@outlook.com>
2023-02-27 02:01:49 -0800

    added level select button

--- Assets/Script/UIManager.cs
+++ Assets/Script/UIManager.cs
@@ -5,17 +5,62 @@ using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour
 {
+    public GameObject StartButton;
+    public GameObject SelectLevelButton;
+    public GameObject QuitButton;
+    public GameObject LevelSelectCanvas;
     // Start is called before the first frame update
+    void Start(){
+        BackToMain();
+    }
     public void GameStart(){
-        SceneManager.LoadScene("Scene1");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
     }
-    void ShowCredit(){
 
+    public void SelectLevel(){
+        StartButton.SetActive(false);
+        QuitButton.SetActive(false);
+        SelectLevelButton.SetActive(false);
+
+        LevelSelectCanvas.SetActive(true);
     }
+
     void Setting(){
 
     }
-    void SelectLevel(){
+
+    void ShowCredit(){
 
     }
+
+    public void BackToMain(){
+        StartButton.SetActive(true);
+        QuitButton.SetActive(true);
+        SelectLevelButton.SetActive(true);
+
+        LevelSelectCanvas.SetActive(false);
+    }
+
+    public void QuitGame(){
+        Application.Quit();
+    }
+
+    // Load levels
+
+    public void LoadScene1(){
+        SceneManager.LoadScene(1);
+    }
+
+    public void LoadScene2(){
+        SceneManager.LoadScene(2);
+    }
+
+    public void LoadScenn3(){
+        SceneManager.LoadScene(3);
+    }
+
+    public void LoadScene4(){
+        SceneManager.LoadScene(4);
+    }
+
 }
