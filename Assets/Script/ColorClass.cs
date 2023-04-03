64ac20a4233507a08ea1f53077bdbb2ff1bb30fa
Zoraaaa <rongyu923826@outlook.com>
2023-04-03 02:36:51 -0700

    Updated enemy collider

--- Assets/Script/ColorClass.cs
+++ Assets/Script/ColorClass.cs
@@ -5,24 +5,24 @@ using static UnityEngine.ColorUtility;
 
 public class ColorClass : MonoBehaviour
 {
-    public colorBlue = new Color32(0, 136, 255, 255);
+    public Color colorBlue = new Color32(0, 136, 255, 255);
     // Blue: Hexadecimal: #0088FF
 
-    public colorYellow = new Color32(255, 240, 0, 255);
+    public Color colorYellow = new Color32(255, 240, 0, 255);
     // Yellow: Hexadecimal: #FFF000
 
-    public colorGreen = new Color32(50, 220, 85, 255);
+    public Color colorGreen = new Color32(50, 220, 85, 255);
     // Green: Hexadecimal: #32DC55
 
-    public colorRed = new Color32(167, 16, 16, 255);
+    public Color colorRed = new Color32(167, 16, 16, 255);
     // Red: Hexadecimal: #A60F0F
 
-    public colorPurple = new Color32(112, 51, 136, 255);
+    public Color colorPurple = new Color32(112, 51, 136, 255);
     // Purple: Hexadecimal: #703388
 
-    public colorOrange = new Color32(238, 96, 51,255);   
+    public Color colorOrange = new Color32(238, 96, 51, 255);   
     // Organe: Hexadecimal: #EE6033
 
-    public colorBlack = new Color32(0, 0, 0, 255);
+    public Color colorBlack = new Color32(0, 0, 0, 255);
     // Black Hexadecimal: #000000
 }
