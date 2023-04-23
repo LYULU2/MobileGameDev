566865b8f73b498bc662b81e24912725ba8f2b8d
Luer LYU <luerlyu@Luers-MacBook-Pro.local>
2023-04-22 18:17:31 -0700

    gun color change sprites

--- Assets/Script/GunColorBehave.cs
+++ Assets/Script/GunColorBehave.cs
@@ -5,6 +5,14 @@ using UnityEngine;
 public class GunColorBehave : MonoBehaviour
 {
     private PlayerBehaviour pb;
+	[SerializeField] 
+	private Sprite defaultGunSprite;
+	[SerializeField] 
+	private Sprite yellowGunSprite;
+	[SerializeField] 
+	private Sprite redGunSprite;
+	[SerializeField] 
+	private Sprite blueGunSprite;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,6 +26,7 @@ public class GunColorBehave : MonoBehaviour
         Queue<int> queue = pb.getColorQueue();
         if (queue.Count == 0)
         {
+			this.GetComponent<SpriteRenderer>().sprite = defaultGunSprite;
             // use the default black sprite
             Debug.Log("Black gun");
         }
@@ -27,16 +36,19 @@ public class GunColorBehave : MonoBehaviour
             if (c == 0)
             {
                 //blue 
+				this.GetComponent<SpriteRenderer>().sprite = blueGunSprite;
                 Debug.Log("Blue gun");
             }
             else if (c == 1)
             {
                 //yellow
+				this.GetComponent<SpriteRenderer>().sprite = yellowGunSprite;
                 Debug.Log("yellow gun");
             }
             else if (c == 2)
             {
                 //red
+				this.GetComponent<SpriteRenderer>().sprite = redGunSprite;
                 Debug.Log("red gun");
             }
         }
